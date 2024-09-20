f9bff107b79fe1a9ecb29cae1a8e3a38ac0220b6
theophiluskibet <kibettheophilus@gmail.com>
2024-09-20 12:28:27 +0300

    setup room db

---
+++ composeApp/src/commonMain/kotlin/com/theophiluskibet/daftari/data/local/database/NotesDatabase.kt
@@ -0,0 +1,22 @@
+package com.theophiluskibet.daftari.data.local.database
+
+import androidx.room.ConstructedBy
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.RoomDatabaseConstructor
+import androidx.room.TypeConverters
+import com.theophiluskibet.daftari.data.local.converters.ListConverter
+import com.theophiluskibet.daftari.data.local.dao.NotesDao
+import com.theophiluskibet.daftari.data.local.entity.NoteEntity
+
+@Database(entities = [NoteEntity::class], version = 1, exportSchema = false)
+@TypeConverters(ListConverter::class)
+@ConstructedBy(NotesDatabaseConstructor::class)
+abstract class NotesDatabase : RoomDatabase() {
+    abstract fun notesDao(): NotesDao
+}
+
+@Suppress("NO_ACTUAL_FOR_EXPECT")
+expect object NotesDatabaseConstructor : RoomDatabaseConstructor<NotesDatabase> {
+    override fun initialize(): NotesDatabase
+}
