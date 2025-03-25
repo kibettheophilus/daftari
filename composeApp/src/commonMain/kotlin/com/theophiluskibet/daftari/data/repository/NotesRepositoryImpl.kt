dbb744d3768aa01dfe184d302fdab4442a8d3233
theophiluskibet <kibettheophilus@gmail.com>
2025-03-25 12:37:11 +0300

    add sample repo and impl

--- composeApp/src/commonMain/kotlin/com/theophiluskibet/daftari/data/repository/NotesRepositoryImpl.kt
+++ composeApp/src/commonMain/kotlin/com/theophiluskibet/daftari/data/repository/NotesRepositoryImpl.kt
@@ -1,10 +1,18 @@
 package com.theophiluskibet.daftari.data.repository
 
 import com.theophiluskibet.daftari.data.local.dao.NotesDao
+import com.theophiluskibet.daftari.data.local.entity.NoteEntity
 import com.theophiluskibet.daftari.domain.NotesRepository
 import org.koin.core.annotation.Single
 
 @Single
 class NotesRepositoryImpl(private val notesDao: NotesDao): NotesRepository {
+    override suspend fun addNote() {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getNotes(): List<NoteEntity> {
+        TODO("Not yet implemented")
+    }
 
 }
--- composeApp/src/commonMain/kotlin/com/theophiluskibet/daftari/domain/NotesRepository.kt
+++ composeApp/src/commonMain/kotlin/com/theophiluskibet/daftari/domain/NotesRepository.kt
@@ -1,4 +1,9 @@
 package com.theophiluskibet.daftari.domain
 
+import com.theophiluskibet.daftari.data.local.entity.NoteEntity
+
 interface NotesRepository {
+    suspend fun addNote()
+    suspend fun getNotes(): List<NoteEntity>
+    //suspend fun
 }
