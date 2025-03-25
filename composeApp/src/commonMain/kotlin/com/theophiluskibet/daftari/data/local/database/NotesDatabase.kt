67cf9a9dc4ec026dc132eb9e72ea0a6f0a680141
theophiluskibet <kibettheophilus@gmail.com>
2025-03-25 12:54:28 +0300

    add database constructor

--- composeApp/src/commonMain/kotlin/com/theophiluskibet/daftari/data/local/database/NotesDatabase.kt
+++ composeApp/src/commonMain/kotlin/com/theophiluskibet/daftari/data/local/database/NotesDatabase.kt
@@ -1,7 +1,9 @@
 package com.theophiluskibet.daftari.data.local.database
 
+import androidx.room.ConstructedBy
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.RoomDatabaseConstructor
 import androidx.room.TypeConverters
 import com.theophiluskibet.daftari.data.local.converters.ListConverter
 import com.theophiluskibet.daftari.data.local.dao.NotesDao
@@ -9,18 +11,12 @@ import com.theophiluskibet.daftari.data.local.entity.NoteEntity
 
 @Database(entities = [NoteEntity::class], version = 1, exportSchema = false)
 @TypeConverters(ListConverter::class)
-abstract class NotesDatabase : RoomDatabase(), DB {
+@ConstructedBy(AppDatabaseConstructor::class)
+abstract class NotesDatabase : RoomDatabase() {
     abstract fun notesDao(): NotesDao
-
-    override fun clearAllTables() {
-        super.clearAllTables()
-    }
 }
 
-/**
- * Hack copied from
- * https://github.com/joreilly/FantasyPremierLeague
- */
-interface DB {
-    fun clearAllTables() {}
+@Suppress("NO_ACTUAL_FOR_EXPECT")
+expect object AppDatabaseConstructor: RoomDatabaseConstructor<NotesDatabase>{
+    override fun initialize(): NotesDatabase
 }
